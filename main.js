b71870df0b6c55352cd7cf8b70637a0a8d987bdf
Omi <omer257@gmail.com>
2017-12-19 16:58:03 +0200

    Added more data

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
 //write your code here to make the tests pass
+var TennisGame = function (name1, name2) {
+  this.playerOneScore = 0;
+  this.playerTwoScore = 0;
+
+  this.playerOneScores = function() { 
+  };
+  this.playerTwoScores = function() { 
+  };
+  this.isDeuce = function () {
+       
+  };
+  this.playerWithHighestScore = function () {
+    
+  };
+  this.hasWinner = function () {
+     
+  };
+  this.hasAdvantage = function () {
+     
+  };
+  this.translateScore = function (score) {
+     
+  };
+  this.getScore = function () {
+      
+  };
+};
