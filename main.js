c8b5076664b2f83a471ce5220fecad9c33f2a9f2
Sima Margulis Davidi <simamarg@gmail.com>
2017-12-25 16:06:00 +0200

    started TennisGame function

--- main.js
+++ main.js
@@ -3,26 +3,39 @@ var TennisGame = function (name1, name2) {
   this.playerOneScore = 0;
   this.playerTwoScore = 0;
 
-  this.playerOneScores = function() { 
+  this.playerOneScores = function () {
+    this.playerOneScore++;
   };
-  this.playerTwoScores = function() { 
+  this.playerTwoScores = function () {
+    this.playerTwoScore++;
   };
+
   this.isDeuce = function () {
-       
+    if (this.playerOneScore >= 3 && this.playerOneScore === this.playerTwoScore) {
+      return true;
+    } else {
+      return false;
+    }
   };
+
   this.playerWithHighestScore = function () {
-    
+    if (this.playerOneScore > this.playerTwoScore) {
+      return name1;
+    } else {
+      return name2;
+    }
   };
+
   this.hasWinner = function () {
-     
+
   };
   this.hasAdvantage = function () {
-     
+
   };
   this.translateScore = function (score) {
-     
+
   };
   this.getScore = function () {
-      
+
   };
 };
